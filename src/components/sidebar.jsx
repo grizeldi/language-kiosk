a381bf69cbf2524a72cc9683217ced091f153636
grizeldi <grizeldi@gmail.com>
2022-05-05 11:38:51 +0200

    Spanish translation and manual language switching

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,17 +1,32 @@
+import i18next from 'i18next';
 import React from 'react';
 import { withTranslation } from 'react-i18next';
 import './sidebar.css';
 
 class Sidebar extends React.Component {
+    changeLanguage(language) {
+        switch (language) {
+            case "english":
+                i18next.changeLanguage("en");
+                break;
+            case "slovenian":
+                i18next.changeLanguage("si");
+                break;
+            case "spanish":
+                i18next.changeLanguage("es");
+                break;
+        }
+    }
+
     render() {
         return (
             <aside>
                 <img src="logo192.png" alt="Gallery logo" className="LogoImg" />
                 <div className="info">
                     <ul>
-                        <li><img src="slovenia.png" alt="SlovenianFlag" className="flag" /></li>
-                        <li><img src="spain.png" alt="SpanishFlag" className="flag" /></li>
-                        <li><img src="united-kingdom.png" alt="FlagOfUK" className="flag" /></li>
+                        <li onClick={() => this.changeLanguage("slovenian")}><img src="slovenia.png" alt="SlovenianFlag" className="flag" /></li>
+                        <li onClick={() => this.changeLanguage("english")}><img src="united-kingdom.png" alt="FlagOfUK" className="flag" /></li>
+                        <li onClick={() => this.changeLanguage("spanish")}><img src="spain.png" alt="SpanishFlag" className="flag" /></li>
                     </ul>
                 </div>
             </aside>
--- src/translations.js
+++ src/translations.js
@@ -18,10 +18,10 @@ export const translations = {
         },
         es: {
             translation: {
-                title: "",
-                author: "",
-                date: "",
-                description: ""
+                title: "Los campos de Spodnji Duplek",
+                author: "Autor: Janez Novak",
+                date: "Pintado en 1876",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut tortor pretium viverra suspendisse potenti nullam ac tortor. Vel eros donec ac odio. Rutrum quisque non tellus orci ac auctor augue. Viverra orci sagittis eu volutpat odio. Id diam vel quam elementum pulvinar etiam non. Molestie nunc non blandit massa enim nec dui nunc mattis. In fermentum et sollicitudin ac. Aliquam eleifend mi in nulla posuere sollicitudin. Mattis pellentesque id nibh tortor id aliquet lectus proin. Lectus urna duis convallis convallis tellus id interdum velit laoreet. Lacus suspendisse faucibus interdum posuere lorem ipsum dolor. Ut tortor pretium viverra suspendisse potenti nullam ac tortor vitae.\n\nVel facilisis volutpat est velit egestas dui id ornare arcu. Nisi lacus sed viverra tellus in hac. Aliquam eleifend mi in nulla posuere. Adipiscing diam donec adipiscing tristique. In ante metus dictum at. Scelerisque eu ultrices vitae auctor eu augue ut lectus arcu. Donec ultrices tincidunt arcu non sodales neque sodales ut. Vel risus commodo viverra maecenas accumsan. In nisl nisi scelerisque eu ultrices. Sed id semper risus in hendrerit gravida rutrum quisque. Dignissim diam quis enim lobortis."
             }
         }
     },
