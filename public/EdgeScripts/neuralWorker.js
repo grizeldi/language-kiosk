925c7592550585908bc7a30ae4a701a238165e63
grizeldi <grizeldi@gmail.com>
2022-05-18 20:03:13 +0200

    Some code cleanup and testing

--- public/EdgeScripts/neuralWorker.js
+++ public/EdgeScripts/neuralWorker.js
@@ -23,13 +23,13 @@ onmessage = (e) => {
 async function classify(data) {
     data = data.map(function(x){return Math.round(x*32767)});
     
-    var classifier = new EdgeImpulseClassifier();
+    const classifier = new EdgeImpulseClassifier();
     await classifier.init();
     //let features = data.split(',').map(x => Number(x.trim()));
-    let res = classifier.classify(data);
+    const res = classifier.classify(data);
     
-    best = "";
-    max = 0;
+    let best = "";
+    let max = 0;
     res.results.forEach(element => {
         if (element.value >= max) {
             best = element.label;
@@ -37,6 +37,7 @@ async function classify(data) {
         }
     });
 
-    console.log(res.results);
+    // console.log(res.results);
+    console.log(best + " " + max);
     //postMessage([dict[best]]);
 }
