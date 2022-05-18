3d383a446dbff7b14afeec39582cdf170264e6c1
grizeldi <grizeldi@gmail.com>
2022-05-18 19:55:11 +0200

    Reusable audio buffer

--- public/EdgeScripts/audio-processor.js
+++ public/EdgeScripts/audio-processor.js
@@ -1,26 +1,34 @@
+class AudioProcessor extends AudioWorkletProcessor {
+  constructor() {
+    super();
+    this.chunk = [];
+    this.timer = 0;
+    this.listeners = [];
 
-class AudioProcessor extends AudioWorkletProcessor { 
-  
-    constructor() {  
-      super();
-      this.chunk = [];
-      this.chunkLength = 0;
-      this.listeners = [];
+    // Constants
+    this.chunkSize = sampleRate; // How many samples do we want in the buffer
+    this.windowLength = 0.5; // How many seconds need to pass between 2 classification attempts.
+  }
+
+  process(inputs, outputs, parameters) {
+    let T = 1 / sampleRate;
+    this.addToChunk(inputs[0][0]);
+    this.timer += T * inputs[0][0].length;
+
+    if (this.timer > this.windowLength) {
+      this.port.postMessage(this.chunk);
+      this.timer = 0;
     }
-    
-    process (inputs, outputs, parameters) {
-      let T = 1/sampleRate;
-      this.chunk.push(...inputs[0][0]);
-      this.chunkLength += T*128;
 
-      if(this.chunkLength > 1){
-        this.port.postMessage(this.chunk);
-        this.chunk = [];
-        this.chunkLength = 0;
-      }
+    return true;
+  }
 
-      return true;
+  addToChunk(data) {
+    this.chunk.push(...data);
+    if (this.chunk.length > this.chunkSize){
+      this.chunk.splice(0, this.chunk.length - this.chunkSize);
     }
   }
-  
-  registerProcessor('audio-processor', AudioProcessor)
+}
+
+registerProcessor('audio-processor', AudioProcessor);
